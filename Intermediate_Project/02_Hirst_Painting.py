a247af0612b96a92bb51bdf545f03b7e0c73c1c6
Aryan-chaudhari-107 <bunny.reg.6531@gmail.com>
2025-06-16 15:47:04 +0530

    add 02_hirst painting

---
+++ Intermediate_Project/02_Hirst_Painting.py
@@ -0,0 +1,38 @@
+import turtle as t
+import random
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+t.colormode(255)
+tim = t.Turtle()
+tim.shape("arrow")
+tim.speed(100)
+tim.penup()
+tim.hideturtle()
+
+tim.setheading(225)
+tim.penup()
+tim.forward(300)
+tim.setheading(0)
+num_of_dots = 100
+
+for count in range(1, num_of_dots+1):
+    tim.color(random_color())
+    tim.dot(20)
+    tim.forward(50)
+
+    if count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+screen = t.Screen()
+screen.exitonclick()
