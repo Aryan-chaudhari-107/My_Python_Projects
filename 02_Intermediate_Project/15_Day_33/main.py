458044f33c1b9be702ad2d38b31e1e088c1abed2
Aryan-chaudhari-107 <bunny.reg.6531@gmail.com>
2025-07-23 12:00:40 +0530

    Using lat and lng find sunrise and sunset time in my area

--- 02_Intermediate_Project/15_Day_33/main.py
+++ 02_Intermediate_Project/15_Day_33/main.py
@@ -4,16 +4,37 @@
 # If response code is 4XX: YOU SCREWED UP
 # If response code is 5XX: I SCREWED UP (for more you can visit httpstatuses.com)
 
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+#
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+#
+# iss_position = (longitude, latitude)
+# print(iss_position)
+
 import requests
+from datetime import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+MY_LAT = 23.526163
+MY_LNG = 72.360352
+
+parameters = {
+    "lat" : MY_LAT,
+    "lng" : MY_LNG,
+    "formatted" : 0
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-
 data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
+print(sunrise)
+print(sunset)
 
-iss_position = (longitude, latitude)
-print(iss_position)
-
+time_now = datetime.now()
+print(time_now)
