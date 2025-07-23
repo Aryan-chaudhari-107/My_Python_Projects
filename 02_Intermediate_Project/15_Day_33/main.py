1fbd2f1d84c6879e9b6c37906a1cf2109e52bf78
Aryan-chaudhari-107 <bunny.reg.6531@gmail.com>
2025-07-23 11:26:45 +0530

    Created Kanye quote using API, requests, Tk inter

--- 02_Intermediate_Project/15_Day_33/main.py
+++ 02_Intermediate_Project/15_Day_33/main.py
@@ -4,15 +4,16 @@
 # If response code is 4XX: YOU SCREWED UP
 # If response code is 5XX: I SCREWED UP (for more you can visit httpstatuses.com)
 
-import requests
+# import requests
+#
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+#
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+#
+# iss_position = (longitude, latitude)
+# print(iss_position)
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-
-data = response.json()
-
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
-
-iss_position = (longitude, latitude)
-print(iss_position)
